fab20ad52351da24e72ef8dc12a62deac4833d83
SteveDev <stevehoneck@gmail.com>
2021-07-21 13:29:48 -0400

    Added <Text> to display if there are no stores in the <StoreList>.

--- components/StoreListComponent.js
+++ components/StoreListComponent.js
@@ -1,16 +1,24 @@
 import React from 'react';
+import { Text } from 'react-native';
+
 import StoreListItem from './StoreListItemComponent';
 
-//Operates the same as "ListComponent", could be combined into a more general function.
+//Operates the same as "ListComponent", could be combined into a more general function depending on how "ListComponent" uses <SectionList>.
 
 function StoreList( {storesArray, storeSelect} ) { //receives array of stores "storesArray" & function "storeSelect" as props from main, passes each object in the array & a copy of the "storeSelect" function to "StoreListItemComponent". These are destructured from "props" so that "storeSelect" does not have to get pulled out of the "props" via an extra line of code "const storeSelect = props.storeSelect;" so it can be passed to "StoreListItem". "storeSelect" can't be passed through the "map" method because that method only takes in items from the "storesArray", objects in that array do not contain the "storeSelect" function.
-    storelist = storesArray.map ( storeObject => { //map function creates a new array called "storeslist" by taking each object from the "storesArray" (which was passed as props from "MainComponent"), renames it "storesObject" and runs the inner "return" function on that item. The result is an array of <StoreListItem>s.
-        return (//The map function renders a <StoreListItem> for each object (which is being called "storeObject") in the "storesArray" and passes values from that object to <StoreListItem> as props ("key", "storeObject", "storeSelect" are packaged into an object called "props" and passed)
-            <StoreListItem key={storeObject.id} storeObject={storeObject} storeSelect={storeSelect} /> 
-        )
-    })
+    if (storesArray.length === 0){ //If there are no stores in the "storesArray" passed from "MainComponent", display the <Text>
+        return <Text>Add a store</Text>
+    } else { //If there are stores in the "storesArray" passed from "MainComponent", "map" them into <StoreListItem>s.
+    
+        storelist = storesArray.map ( storeObject => { //map function creates a new array called "storeslist" by taking each object from the "storesArray" (which was passed as props from "MainComponent"), renames it "storesObject" and runs the inner "return" function on that item. The result is an array of <StoreListItem>s.
+            return (//The map function renders a <StoreListItem> for each object (which is being called "storeObject") in the "storesArray" and passes values from that object to <StoreListItem> as props ("key", "storeObject", "storeSelect" are packaged into an object called "props" and passed)
+                <StoreListItem key={storeObject.id} storeObject={storeObject} storeSelect={storeSelect} /> 
+            )
+        })
 
-    return storelist; //New array of <StoreListItems> created by "map" function is returned by the "StoreList" function
+        return storelist; //New array of <StoreListItems> created by "map" function is returned by the "StoreList" function
+    }
 }
+
 
 export default StoreList;
