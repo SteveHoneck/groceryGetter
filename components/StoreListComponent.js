670d81b04d8db7c408295193e9fcfb28da6bba81
SteveDev <stevehoneck@gmail.com>
2021-07-28 07:08:44 -0400

    Update store list so selecting a store highlights more background than just the background behind the text.

--- components/StoreListComponent.js
+++ components/StoreListComponent.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { Text, FlatList } from 'react-native';
+import { Text, FlatList, StyleSheet } from 'react-native';
+import { View } from 'react-native-animatable';
 import StoreListItem from './StoreListItemComponent';
 
 //Operates the same as "ListComponent", could be combined into a more general function depending on how "ListComponent" uses <SectionList>.
 
 function StoreList( {storesArray, storeSelect} ) { //receives array of stores "storesArray" & function "storeSelect" as props from main, passes each object in the array & a copy of the "storeSelect" function to "StoreListItemComponent" through <FlatList>'s built-in functionality. 
     const renderItem = ({ item }) => ( //"renderItem" is called by the <FlatList>'s "renderItem" property
-        <StoreListItem storeObject={item} storeSelect={storeSelect} /> // The <FlatList>'s "renderItem" property takes an object from the "data" property, names it "item" and passes it to this function which renders a <StoreListItem> for each "item" (object in the "data" property), and passes to the <StoreListItem> the "item" renamed to "storeObject" and the "storeSelect" function as props.
+        <StoreListItem storeObject={item} storeSelect={storeSelect} /* The <FlatList>'s "renderItem" property takes an object from the "data" property, names it "item" and passes it to this function which renders a <StoreListItem> for each "item" (object in the "data" property), and passes to the <StoreListItem> the "item" renamed to "storeObject" and the "storeSelect" function as props.*/ /> 
     )
 
     return(
@@ -14,11 +15,20 @@ function StoreList( {storesArray, storeSelect} ) { //receives array of stores "s
             data={storesArray} // Array from which each object is passed to "render" item
             renderItem={renderItem} // Function to be called on each object in the "data" property
             keyExtractor={item => item.id} //
-            ListEmptyComponent={<Text style={{color: 'darkgray'}}>No stores</Text>} //Displays when there are no stores in the "storesArray"
-            contentContainerStyle={{alignItems: 'center', width: 125}} //'center' aligns the store names in the list to the center of the list. 'width' sets the width of the list, without this property, the width of the list would snap to the length of the longest store
+            ListEmptyComponent={<Text style={styles.listEmptyText}>No stores</Text>} //Displays when there are no stores in the "storesArray"
+            contentContainerStyle={styles.contentContainer} 
         />
     )
 }
 
+const styles = StyleSheet.create({
+    listEmptyText: {
+        color: 'darkgray'
+    },
+    contentContainer: {
+        width: 125, //Sets the width of the list, without this property, the width of the list would snap to the length of the longest store
+        alignItems: 'stretch', //Causes items in the list to take up entire width 
+    }
+})
 
 export default StoreList;
--- components/StoreListItemComponent.js
+++ components/StoreListItemComponent.js
@@ -5,7 +5,7 @@ const StoreListItem = (props) => { //props will be "storeSelect" function and on
 
     return(
         <TouchableOpacity style={{backgroundColor: props.storeObject.backgroundColor}} onPress={() => props.storeSelect(props.storeObject.storeName)} /*Makes the child component go slightly transparent when touched. Style "backgroundColor" is taken from the object that was passed from the map function in "StoreListComponent", this style can be changed through the "storeSelect" function that will be run when this <StoreListItem> is touched: Runs the "storeSelect" function that is in "MainComponent" and passes it the string that is in the "storeName" value from the object that was passed from the map function in "StoreListComponent" which tells the "storeSelect" function to update the style properties of the object that matches that "storeName" ("storeName" is also used in "addStoreSubmit" function). When the style properties are updated, the entire list re-renders since the state changed, which in turn displays the new styles */> 
-            <Text style={{color: props.storeObject.color}} >{props.storeObject.storeName}</Text>
+            <Text style={[{color: props.storeObject.color}, styles.text]} >{props.storeObject.storeName}</Text>
         </TouchableOpacity>
     )
 
@@ -13,7 +13,9 @@ const StoreListItem = (props) => { //props will be "storeSelect" function and on
 
 
 const styles = StyleSheet.create({
-
+    text: {
+        textAlign: 'center' //Causes the text to be centered within its 'stretched' container. "StoreListComponent" property "contentContainerStyle" is set to "stretched" which causes the 'box' that the text is in to take up the width of the entire 'container'.
+    }
 })
 
 
