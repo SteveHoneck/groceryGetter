45fc614815e5e124bec86da53b6aa440e8813481
SteveDev <stevehoneck@gmail.com>
2021-08-01 09:56:38 -0400

    Rework how message is displayed when no items are present for a particular store.

--- components/ListComponent.js
+++ components/ListComponent.js
@@ -5,28 +5,35 @@ import * as Animatable from 'react-native-animatable';
 import { LinearGradient } from 'expo-linear-gradient';
 
 function List( {itemArray, storesArray, checkBoxToggle} ) { //receives array of items "itemArray", array of stores "storesArray" & function "checkBoxToggle" as props from main. "for" loop combines the "itemArray" and "storesArray" into an array called "sectionListArray" that can be used by <SectionList>. "checkBoxToggle" is passed directly to "ListItemComponent". 
+    
+    //Start <SectionList> array build. Builds an array to be used by <SectionList> (Should this be in a function?)
     let sectionListArray = [] //Initalize an empty array to be used by <SectionList>
     let sectionListObject = []
 
     for(let i = 0; i < storesArray.length; i++) { //"for" loop iterates over the "storesArray" picking out each store name
-        let storeName = storesArray[i].storeName; //"storeName" will be the "storeName" of the current iteration object from "storesArray"
+        let title = storesArray[i].storeName; //"title" will be the "storeName" of the current iteration object from "storesArray"
     
-        let data = itemArray.filter(itemObject => itemObject.storeName === storeName) //Creates an array called "data" that is an array of objects from the "itemArray" matching the current store name
-    
-        if (data.length === 0) { //if the array "data" has nothing in it, that means that there were no items in the "itemArray" with the "storeName" being iterated over. In this case, return an object with the "storeName" being iterated over & the string 'No Items' in the "data" array. This will cause the "storeName" to be rendered as a section header in the <SectionList> and a message (written out in "ListItemComponent") to be displayed as the "ListItem"
-            sectionListObject = { //Create an object that is in the format that can be used by <FlatList>, a string "title" to be rendered as the list section header and "data" that is an array of objects to be rendered as the list items
-                title: storeName,
-                data: ['No items']
-            } 
-        } else { //If the "itemArray.filter" method found items with the "storeName" being iterated over, put all those item objects in the "data" value for the "sectionListObject"
-            sectionListObject = { //Create an object that is in the format that can be used by <FlatList>, a string "title" to be rendered as the list section header and "data" that is an array of objects to be rendered as the list items
-                title: storeName,
-                data: data
-            }
-        }
+        let data = itemArray.filter(itemObject => itemObject.storeName === title) //Creates an array called "data" that is an array of objects from the "itemArray" whos "storeName" value matches the current store name (which has been renamed 'title')
 
+            sectionListObject = { //Create an object that is in the format that can be used by <FlatList>, a string "title" to be rendered as the list section header and "data" that is an array of objects to be rendered as the list items
+                title, //ES6 syntax - since the key and value are the same, 'title: title' can be written as just 'title'
+                data //ES6 syntax - since the key and value are the same, 'data: data' can be written as just 'data'
+            }
+        
         sectionListArray.push(sectionListObject); //Add the "sectionListObject" to the "sectionListArray"
     }
+    //End <SectionList> array build
+
+    noItemMessage = ({section}) => { //This function is called as the footer to each section (which is the store name). Takes the entire object ('title' as a string and 'data' as an array of object) currently being iterated over by <SectionList>, names it 'section' and passes it into this function. 
+      if(section.data.length == 0){ //if the 'data' array is empty, there are no items for the store, so return a message of <Text>.
+        return  (
+          <Animatable.View animation='slideInLeft' duration={1500}>
+            <Text style={styles.storeEmptyText}>No items for this store...</Text>
+          </Animatable.View>
+        )
+      }
+      return null //If the 'data' array is not empty (there are items for the store), then don't render the "renderSectionFooter" at all
+   }
     
     return(
         <SectionList
@@ -50,7 +57,8 @@ function List( {itemArray, storesArray, checkBoxToggle} ) { //receives array of 
             </Animatable.View>  
             )}
           stickySectionHeadersEnabled={true}
-          ListEmptyComponent={<Text style={styles.listEmptyText}>Add a store to get started!</Text>} //Only shows up when there are no items OR stores in list
+          ListEmptyComponent={<Text style={styles.listEmptyText}>Add a store to get started!</Text>} //Only shows up when there are no items AND stores in list
+          renderSectionFooter={section => noItemMessage(section)} //Will show up if there is a Store with no items. Making this an arrow function as opposed to a direct function call (i.e. 'this.noItemMessage') allows the animation to happen every time the message displays.
           persistentScrollbar={true}//Makes the vertical scroll bar always visible so that user knows if there are more items on list than are currently visible
         />
     )
@@ -71,7 +79,12 @@ const styles = StyleSheet.create({
       textAlign: 'center',
       marginTop: '50%', //Moves the text down. Not sure what the 50% is measured from and as a result, text does not appear in the exact middle of the screen. To make text in the middle, add style "justifyContent: 'center'" to the parent <View> of <List> in main component (however doing that also centers the list of stores and items which is not desired, want those to always start at the top) (could pass that style property back to "MainComponent" on condition that "ListEmptyComponent" is displayed) 
       color: 'black',
-    }
+    },
+    storeEmptyText: {
+      fontSize: 16,
+      marginLeft: 20,
+      color: 'gray',
+  }
   });
 
 export default List;
--- components/ListItemComponent.js
+++ components/ListItemComponent.js
@@ -4,25 +4,18 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { CheckBox } from 'react-native-elements'
 
-const ListItem = (props) => { //props will be "checkBoxToggle" function and one object named "item" from the map function in the "ListComponent" which has been renamed "item". Called as many times as there are item objects in the "data" propery of each "sectionListArray" object from <SectionList> in "ListComponent". DESTRUCTURE PROPS AND REFACTOR CODE BELOW????
-    if (props.item === 'No items') { //This loop will be entered if the "itemArray.filter" method (in "ListComponent") returns nothing (in which case an object is created with the text 'No items'), meaning there were no items that had the "storeName" being iterated over. The <Text> returned will be rendered in the <SectionList> from "ListComponent" as the list item under the "storeName" that had no items.
-        return(
-            <Text style={styles.listEmptyText}>No items for this store...</Text>
-        )
-    } else { //Element to be returned if the "itemArray.filter" method (in "ListComponent") returns multiple
-        return(
-            <View>
-                <CheckBox 
-                    containerStyle={styles.checkBoxRow}
-                    title={props.item.item} //Use the "item" value from the "item" passed from the <SectionList>'s "renderItem" method as the title for the checkbox
-                    checked={props.item.isChecked} //Use the "isChecked" value from the "item" passed from the <SectionList>'s "renderItem" method as the true or false for <CheckBox>'s checked value
-                    onPress={() => props.checkBoxToggle(props.item.id)} //run the "checkBoxToggle" function that was passed from the "ListComponent" <ListItem> call and give the function the value "id" from the "item" passed from the <SectionList>'s "renderItem" as an argument 
-                    textStyle={props.item.textStyle} //Use the "textStyle" value (which is a "styles" object) passed from the <SectionList>'s "renderItem" method as the styles to be applied to the "title" 
-                />
-            </View>
-        )
-    }
-
+const ListItem = ({ item, checkBoxToggle }) => { //props will be "checkBoxToggle" function and one object named "item" from the map function in the "ListComponent" which has been renamed "item". Called as many times as there are item objects in the "data" propery of each "sectionListArray" object from <SectionList> in "ListComponent". DESTRUCTURE PROPS AND REFACTOR CODE BELOW????
+    return (
+        <View>
+            <CheckBox 
+                containerStyle={styles.checkBoxRow}
+                title={item.item} //Use the "item" value from the "item" passed from the <SectionList>'s "renderItem" method as the title for the checkbox
+                checked={item.isChecked} //Use the "isChecked" value from the "item" passed from the <SectionList>'s "renderItem" method as the true or false for <CheckBox>'s checked value
+                onPress={() => checkBoxToggle(item.id)} //run the "checkBoxToggle" function that was passed from the "ListComponent" <ListItem> call and give the function the value "id" from the "item" passed from the <SectionList>'s "renderItem" as an argument 
+                textStyle={item.textStyle} //Use the "textStyle" value (which is a "styles" object) passed from the <SectionList>'s "renderItem" method as the styles to be applied to the "title" 
+            />
+        </View>
+    )
 }
 
 
@@ -32,11 +25,6 @@ const styles = StyleSheet.create({
         marginVertical: 2.5,
         marginLeft: 20, //Left and Right can't be combined to "marginHorizontal" and have proper effect
         marginRight: 20,
-    },
-    listEmptyText: {
-        fontSize: 16,
-        marginLeft: 20,
-        color: 'gray',
     }
 })
 
