ccad9bd214264827418a4ffeb74cb67011ab6485
SteveDev <stevehoneck@gmail.com>
2021-08-19 16:13:54 -0400

    Updated comments.

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -1,7 +1,7 @@
 import * as ActionTypes from './ActionTypes'; //* wildcard, lets you import all named exports from "ActionTypes" at once
 import { baseUrl } from '../shared/baseUrl';
 
-export const fetchItems = () => dispatch => { //Able to use an arrow function wrapped in an arrow function due to enabling Redux Thunk. This allows the "dispatch" method to be passed to the inner function. 'dispatch' comes from
+export const fetchItems = () => dispatch => { //Able to use an arrow function wrapped in an arrow function due to enabling Redux Thunk. This allows the "dispatch" method to be passed to the inner function. 'dispatch' is a built in method from the Redux Store, since 'thunk' is activated in the 'createStore' function via 'applyMiddleware' argument, it gains access to the 'dispatch' method and allows it to be passed here via an arrow function wrapped in an arrow function .
 
     return fetch(baseUrl + 'itemArray')
             .then(response => { //'fetch' returns an 
@@ -19,7 +19,7 @@ export const fetchItems = () => dispatch => { //Able to use an arrow function wr
             }
         )
         .then(response => response.json())
-        .then(itemArray => dispatch(loadItems(itemArray)))
+        .then(itemArray => dispatch(loadItems(itemArray))) //Calling 'dispatch' passes the inner argument ('loadItems') to the 'store' (since 'dispatch' is a built in method available from the Redux store when 'createStore' is called from the Redux library). The store calls the root reducer created by 'combineReducers' in the 'configureStore' file. The root reducer is a combination of all the reducer files, therefore, the 'loadItems' function/action creator is passed to the store, then to all the 'switch' cases in all reducer files, and when matched to a switch case, the appropriate reducer code is ran and the store is updated.
         .catch(error => dispatch(itemsFailed(error.message))); //Catches all and any errors thrown by the promise chain and passes the ".message" property of the error object to the "itemsFailed" function
 };
 
