c73c7a0361c4337229d5d4852f3dd944cdeb836c
SteveDev <stevehoneck@gmail.com>
2021-08-18 22:54:48 -0400

    Updated comments.

--- App.js
+++ App.js
@@ -3,11 +3,11 @@ import Main from './components/MainComponent';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './redux/configureStore';
 
-const store = ConfigureStore();
+const store = ConfigureStore(); //Call the function 'ConfigureStore' from the 'configureStore' file. This creates the Redux store and places it in vairable 'store'
 
 export default function App() {
   return (
-    <Provider store={store}>
+    <Provider store={store} /*'Provider' component makes the Redux store available to all child components, in this case <Main> and all of its children*/>
       <Main />
     </Provider>  
   );
